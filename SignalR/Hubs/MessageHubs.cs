801796a63bb27867c1c8b41cff985df7ce261a8f
Sarthak Dhakal <sarthakdhakal03@gmail.com>
2022-12-01 10:05:53 +0545

    Initial commit

---
+++ SignalR/Hubs/MessageHubs.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.Models;
+
+namespace SignalR.Hubs
+{
+    public class MessageHubs:Hub
+    {
+        public async Task NewMessage(Message message)
+        {
+            await Clients.All.SendAsync("MessageReceived", message);  
+        }
+    }
+}
